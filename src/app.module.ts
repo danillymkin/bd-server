241c1a44ae4bf0908fa67ae81f46817545d496d2
danillyamkin <danilwebdev@gmail.com>
2022-02-07 21:54:47 +0300

    The database is connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logger: 'advanced-console',
+      logging: ['error', 'query'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
