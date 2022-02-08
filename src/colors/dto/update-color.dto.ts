57610af6128ef96d0d6b30d4ba30a4a3c6285cbc
danillyamkin <danilwebdev@gmail.com>
2022-02-08 14:36:05 +0300

    Removed duplicate code

--- src/colors/dto/update-color.dto.ts
+++ src/colors/dto/update-color.dto.ts
@@ -1,9 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
+import { CreateColorDto } from './create-color.dto';
 
-export class UpdateColorDto {
-  @ApiProperty({ example: 'Черный', description: 'Название цвета' })
-  readonly name?: string;
-
-  @ApiProperty({ example: '#000000', description: 'HEX код цвета' })
-  readonly hex?: string;
-}
+export class UpdateColorDto extends PartialType(CreateColorDto) {}
