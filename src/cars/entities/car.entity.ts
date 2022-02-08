0e1d87849f8f04b63b48892df193562e8c9b3bd6
danillyamkin <danilwebdev@gmail.com>
2022-02-08 15:09:16 +0300

    Add documentation

--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -6,21 +6,27 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Specifications } from './specifications.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'cars' })
 export class Car {
+  @ApiProperty({ example: '1' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'BMW X5', name: 'Название' })
   @Column({ nullable: false })
   name: string;
 
+  @ApiProperty({ example: '6 700 000', name: 'Стоимость' })
   @Column({ default: 0, nullable: false })
   price: number;
 
+  @ApiProperty({ name: 'Описание' })
   @Column({ type: 'text' })
   description: string;
 
+  @ApiProperty({ type: () => Specifications, name: 'Характеристики' })
   @OneToOne(() => Specifications)
   @JoinColumn()
   specifications: Specifications;
--- src/cars/entities/specifications.entity.ts
+++ src/cars/entities/specifications.entity.ts
@@ -1,39 +1,51 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { CarBody, CarDrive, CarFuel, CarTransmission } from '../utils/enums';
 import { Color } from '../../colors/entities/color.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'specifications' })
 export class Specifications {
+  @ApiProperty({ example: '1' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: '2017', name: 'Год выпуска' })
   @Column({ nullable: false })
   releaseYear: number;
 
+  @ApiProperty({ example: '32 000', name: 'Пробег' })
   @Column()
   mileage: number;
 
+  @ApiProperty({ enum: CarBody, example: 'Седан', name: 'Кузов' })
   @Column({ type: 'enum', enum: CarBody })
   body: CarBody;
 
+  @ApiProperty({ type: () => Color, name: 'Цвет' })
   @ManyToOne(() => Color, (color: Color) => color.specifications)
   color: Color;
 
+  @ApiProperty({ example: '18 700', name: 'Налог' })
   @Column()
   tax: number;
 
+  @ApiProperty({ example: 'Седан', name: 'Кузов' })
   @Column({ type: 'enum', enum: CarTransmission })
   transmission: CarTransmission;
 
+  @ApiProperty({ enum: CarDrive, example: 'Задний', name: 'Привод' })
   @Column({ type: 'enum', enum: CarDrive })
   drive: CarDrive;
 
+  @ApiProperty({ enum: CarFuel, example: 'Дизель', name: 'Тип топлива' })
   @Column({ type: 'enum', enum: CarFuel })
   fuel: CarFuel;
 
+  @ApiProperty({ example: '250', name: 'Мощность л/с' })
   @Column()
   power: number;
 
+  @ApiProperty({ example: '2.0', type: 'float', name: 'Объем двигателя' })
   @Column({ type: 'float' })
   volume: number;
 }
