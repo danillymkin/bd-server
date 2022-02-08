dce7966a3c4d24603ddf7ad2f5ffadabd7f7357b
danillyamkin <danilwebdev@gmail.com>
2022-02-08 15:39:16 +0300

    Add documentation

--- src/colors/entities/color.entity.ts
+++ src/colors/entities/color.entity.ts
@@ -1,17 +1,25 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Specifications } from '../../cars/entities/specifications.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'colors' })
 export class Color {
+  @ApiProperty({ example: '1' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'Черный', name: 'Название цвета' })
   @Column({ nullable: false })
   name: string;
 
+  @ApiProperty({ example: '#000000', name: 'HEX код цвета' })
   @Column({ nullable: false })
   hex: string;
 
+  @ApiProperty({
+    type: () => [Specifications],
+    name: 'Список характеристик авто',
+  })
   @OneToMany(
     () => Specifications,
     (specifications: Specifications) => specifications.color,
