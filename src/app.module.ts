151150f38dcda7d24cbac79fb5606fbf6469f8dd
danillyamkin <danilwebdev@gmail.com>
2022-02-08 12:21:20 +0300

    Cars module is connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       logger: 'advanced-console',
       logging: ['error', 'query'],
     }),
+    CarsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
