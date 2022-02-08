5a561403c624b8b0f2a789d1413a12d077016f42
danillyamkin <danilwebdev@gmail.com>
2022-02-08 13:07:10 +0300

    Import Color module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarsModule } from './cars/cars.module';
+import { ColorsModule } from './colors/colors.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { CarsModule } from './cars/cars.module';
       logging: ['error', 'query'],
     }),
     CarsModule,
+    ColorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
