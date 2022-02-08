73cceb0559de9f8c8ac04ff5608d71f508a3680d
danillyamkin <danilwebdev@gmail.com>
2022-02-08 22:54:54 +0300

    Added validation

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,16 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CreateSpecificationsDto } from './create-specifications.dto';
+import {
+  IsNotEmptyObject,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateCarDto {
   @ApiProperty({ example: 'BMW X5', name: 'Название' })
+  @IsString({ message: 'Должно быть строкой' })
   readonly name: string;
 
   @ApiProperty({ example: '6 700 000', name: 'Стоимость' })
+  @IsNumber({}, { message: 'Должна быть числом' })
+  @Min(0, { message: 'Должна быть больше нуля' })
   readonly price: number;
 
   @ApiProperty({ name: 'Описание' })
-  readonly description: string;
+  @IsOptional()
+  @IsString({ message: 'Должно быть строкой' })
+  readonly description?: string;
 
   @ApiProperty({ type: () => CreateSpecificationsDto, name: 'Характеристики' })
+  @IsObject({ message: 'Должно быть объектом характеристик' })
+  @IsNotEmptyObject(
+    {},
+    { message: 'Объект характеристик должен быть не пустым' },
+  )
+  @ValidateNested()
+  @Type(() => CreateSpecificationsDto)
   readonly specifications: CreateSpecificationsDto;
 }
---
+++ src/cars/utils/constants.ts
@@ -0,0 +1,5 @@
+export const MIN_YEAR = 1890;
+export const MAX_MILEAGE = 10_000_000;
+export const MAX_TAX = 1_000_000;
+export const MAX_POWER = 10_000;
+export const MAX_VOLUME = 10;
