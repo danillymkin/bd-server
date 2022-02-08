ae31d21bf57975b6f354770d9573a3d7c7ab3179
danillyamkin <danilwebdev@gmail.com>
2022-02-08 15:07:27 +0300

    Description property changed to name

--- src/colors/dto/create-color.dto.ts
+++ src/colors/dto/create-color.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateColorDto {
-  @ApiProperty({ example: 'Черный', description: 'Название цвета' })
+  @ApiProperty({ example: 'Черный', name: 'Название цвета' })
   readonly name: string;
 
-  @ApiProperty({ example: '#000000', description: 'HEX код цвета' })
+  @ApiProperty({ example: '#000000', name: 'HEX код цвета' })
   readonly hex: string;
 }
