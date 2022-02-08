dd035166752f1eb13c9c27d1a354839521968426
danillyamkin <danilwebdev@gmail.com>
2022-02-08 22:54:06 +0300

    Added handling of nested errors

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -5,8 +5,9 @@ import {
   PipeTransform,
 } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
 import { ValidationException } from '../exceptions/validation.exception';
+import { flattenDeep } from 'lodash';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
@@ -14,24 +15,33 @@ export class ValidationPipe implements PipeTransform<any> {
     if (!metadata.metatype || !this.toValidate(metadata.metatype)) {
       return value;
     }
-
     const obj = plainToClass(metadata.metatype, value);
     const errors = await validate(obj);
 
     if (errors.length) {
       const messages = {
         statusCode: HttpStatus.BAD_REQUEST,
-        messages: errors.map((error) => {
-          return `${error.property} - ${Object.values(error.constraints).join(
-            ', ',
-          )}`;
-        }),
+        messages: this.getMessages(errors),
         error: 'Bad Request',
       };
       throw new ValidationException(messages);
     }
 
     return value;
+  }
+
+  private getMessages(errors: ValidationError[]) {
+    return flattenDeep(
+      errors.map((error) => {
+        if (error.children.length > 0) {
+          return this.getMessages(error.children);
+        } else {
+          return `${error.property} - ${Object.values(error.constraints).join(
+            ', ',
+          )}`;
+        }
+      }),
+    );
   }
 
   private toValidate(metatype): boolean {
