1c712c8042608043ab610eeadb325bfca1671e63
danillyamkin <danilwebdev@gmail.com>
2022-02-14 17:57:35 +0300

    Added cookie parser and CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from './pipes/validation.pipe';
 import { NotFoundInterceptor } from './interceptors/not-found.interceptor';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
+
+  app.use(cookieParser());
+
+  app.enableCors({ credentials: true, origin: `${process.env.CLIENT_URL}` });
 
   const PORT = +process.env.PORT || 5000;
   await app.listen(PORT, () => console.log(`Server started on ${PORT} port.`));
