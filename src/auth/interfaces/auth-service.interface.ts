f29f5147875cd11582909910487b5b5b2c2888af
danillyamkin <danilwebdev@gmail.com>
2022-05-13 21:42:27 +0300

    Created auth service interface

---
+++ src/auth/interfaces/auth-service.interface.ts
@@ -0,0 +1,21 @@
+import { User } from '../../users/entities/user.entity';
+import { Request, Response } from 'express';
+import { UserAndToken } from '../../users/types/user-and-token.type';
+import { RegisterUserDto } from '../dto/register-user.dto';
+
+export const AUTH_SERVICE = 'AUTH_SERVICE';
+
+export interface AuthService {
+  login(userData: User, response: Response): Promise<UserAndToken>;
+
+  register(
+    registerUserDto: RegisterUserDto,
+    response: Response,
+  ): Promise<UserAndToken>;
+
+  refresh(refreshToken: string, response: Response): Promise<UserAndToken>;
+
+  logout(refreshToken: string, request): Promise<void>;
+
+  googleLogin(req: Request, res: Response);
+}
